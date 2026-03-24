207bda1e1b044c6275335f9ea09af0727ec45395
Holly Cummins <holly.k.cummins@googlemail.com>
2026-03-24 15:34:41 +0000

    Revert "Change appCpus type from int to String in Resources record (#351)"
    This reverts commit 22417a85511d9fd2430063272b83a216ce352c1c.

--- graphics-generator/src/main/java/io/quarkus/infra/performance/graphics/model/Resources.java
+++ graphics-generator/src/main/java/io/quarkus/infra/performance/graphics/model/Resources.java
@@ -3,7 +3,7 @@ package io.quarkus.infra.performance.graphics.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record Resources(
-    @JsonProperty("app_cpus") String appCpus,
+    @JsonProperty("app_cpus") int appCpus,
     Cpu cpu
 ) {
 
